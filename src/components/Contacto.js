84157aedb4290846e0059168ceaf21325eb4952b
ErikRobles <erikjames69@hotmail.com>
2019-04-16 12:05:20 -0500

    set rel paths

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -12,7 +12,7 @@ const Styles = styled.div`
    }
 `;
 
-const API_PATH = 'http://www.rrspark.com/psicoterapiaviajeinterior/api/contact/index.php';
+const API_PATH = 'https://psicoterapiaviajeinterior.herokuapp.com/api/contact/index.php';
 console.log(API_PATH);
 class Contacto extends Component {
   constructor (props) {
