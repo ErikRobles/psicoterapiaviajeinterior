b5e7c129ca8d2965e0b85a5f89abbbad937bfdcd
ErikRobles <erikjames69@hotmail.com>
2019-04-15 19:39:54 -0500

    added express.static to service worker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -52,6 +52,15 @@ export function register(config) {
       }
     });
   }
+
+  if(process.env.NODE_ENV === 'production') {
+    // Set Static Folder
+    app.use(express.static('client/build'));
+  } 
+  app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+
 }
 
 function registerValidSW(swUrl, config) {
