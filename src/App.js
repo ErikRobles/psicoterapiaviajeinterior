1d392262af6e84b5b579203c353d5d969d062970
ErikRobles <erikjames69@hotmail.com>
2019-04-15 23:33:22 -0500

    another change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import ViajeInterior from './components/ViajeInterior';
-import Terapia from './components/Terapia';
-import PreguntasFrecuentes from './components/PreguntasFrecuentes';
-import Contacto from './components/Contacto';
-import Navbar from './components/CustomNavBar';
-import Footer from './components/Footer';
-import CV from './components/CV';
+import ViajeInterior from 'psicoterapiaviajeinterior/components/ViajeInterior';
+import Terapia from 'psicoterapiaviajeinterior/components/Terapia';
+import PreguntasFrecuentes from 'psicoterapiaviajeinterior/components/PreguntasFrecuentes';
+import Contacto from 'psicoterapiaviajeinterior/components/Contacto';
+import Navbar from 'psicoterapiaviajeinterior/components/CustomNavBar';
+import Footer from 'psicoterapiaviajeinterior/components/Footer';
+import CV from 'psicoterapiaviajeinterior/components/CV';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 
