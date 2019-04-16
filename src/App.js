46abd3e9906f75af8388f117a4f9eaa8c8a32ea5
ErikRobles <erikjames69@hotmail.com>
2019-04-16 11:52:34 -0500

    set rel paths

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import ViajeInterior from '/components/ViajeInterior';
-import Terapia from '/components/Terapia';
-import PreguntasFrecuentes from '/components/PreguntasFrecuentes';
-import Contacto from '/components/Contacto';
-import Navbar from '/components/CustomNavBar';
-import Footer from '/components/Footer';
-import CV from '/components/CV';
+import ViajeInterior from './components/ViajeInterior';
+import Terapia from './components/Terapia';
+import PreguntasFrecuentes from './components/PreguntasFrecuentes';
+import Contacto from './components/Contacto';
+import Navbar from './components/CustomNavBar';
+import Footer from './components/Footer';
+import CV from './components/CV';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 
